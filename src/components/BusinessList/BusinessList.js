0e274adb9f2abbca62eb1988427edcc39eebd6b0
Svitlana Mishchuk <sveta.mishchuk@gmail.com>
2019-02-11 13:43:30 +0200

    noticket, added Business, BusinessList and SearchBox component with static data

---
+++ src/components/BusinessList/BusinessList.js
@@ -0,0 +1,22 @@
+/**
+ * Created by svitlanamishchuk on 2/11/19.
+ */
+import React from 'react';
+import './BusinessList.css';
+import Business from '../Business/Business';
+
+class BusinessList extends React.Component {
+    render() {
+        return (
+            <div className="BusinessList">
+                <Business/>
+                <Business/>
+                <Business/>
+                <Business/>
+                <Business/>
+                <Business/>
+            </div>
+        )
+    }
+}
+export default BusinessList;
